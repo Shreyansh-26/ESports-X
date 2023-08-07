4c38ce03e96e575d6a7a713864aebab92762cf0a
Shreyansh Shukla <williamtiger135@gmail.com>
2023-08-07 16:00:08 +0530

    frist commit

---
+++ website sample/website.js
@@ -0,0 +1,12 @@
+let dubaitext = document.getElementById('dubaitext');
+let burjkhalifa = document.getElementById('burjkhalifa');
+let stars = document.getElementById('stars');
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+
+    dubaitext.style.left = value*-2+'px';
+    burjkhalifa.style.top = value*1+'px';
+    stars.style.top = value*1+'px';
+
+} );
